17ca04d8116be98ec574d667f507b2a533beb714
ZhiHuangLau <lauzhihuang@gmail.com>
2025-01-08 13:38:24 +0800

    Add board status code
    This commit adds the js code to create the board status.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const boardStatus= (function(){
+  const topLeft="mek mek";
+  const topCenter="";
+  const topRight="";
+  const middleLeft="";
+  const middleCenter="";
+  const middleRight="";
+  const bottomLeft="";
+  const bottomCenter="";
+  const bottomRight="";
+  return {topLeft,topCenter,topRight,middleLeft,middleCenter,middleRight,bottomLeft,bottomCenter,bottomRight};
+})();
 
+const mek=document.getElementById("top-left");
+
+mek.textContent=boardStatus.topLeft;
