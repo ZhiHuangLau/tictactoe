d9e5abe6d050055787f6e18c5c1323831223e387
ZhiHuangLau <lauzhihuang@gmail.com>
2025-01-09 11:29:23 +0800

    Add function showing board state as num array
    This commit adds a function to show the current board state as a
    numerical array. This is to make the later process of finding a winner
    easier.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*object representing current state of the boards*/
 const boardStatus= (function(){
   const topLeft="mek mek";
   const topCenter="";
@@ -11,16 +13,35 @@ const boardStatus= (function(){
   return {topLeft,topCenter,topRight,middleLeft,middleCenter,middleRight,bottomLeft,bottomCenter,bottomRight};
 })();
 
+/* Functions to do stuff with the board*/
 const boardFunctions=(function() {
+  /* Returns an object showing only the values on the board right now*/
   const currentBoardState=Object.values(boardStatus);
+  
+  /*Function to generate a numerical array that shows board state*/
+  const smh=(function()
+    {const tempArr=[];
+    for (let i = 0; i < currentBoardState.length; i++) {
+    if (currentBoardState[i]=="X") {
+      const element = 1;
+      tempArr.push(element);
+    }else if (currentBoardState[i]=="O"){
+      const element = -1;
+      tempArr.push(element);
+    }else {
+      const element = 0;
+      tempArr.push(element);
+    }}
+    return(tempArr)
+  })();
 
-
-  return {currentBoardState};
+  return {currentBoardState,smh};
 })();
 
 const mek=document.getElementById("top-left");
 
 mek.textContent=boardStatus.topLeft;
+/* Function to reset the board to empty*/
 function resetBoard(){
   boardStatus.topLeft="";
   boardStatus.topCenter="";
@@ -33,9 +54,10 @@ function resetBoard(){
   boardStatus.bottomRight="";
 };
 
+/* Function to create player*/
 function createPlayer(name) {
   const playerName=name;
-  let score = 0;
+  let score = -1;
   const getScore= () => score;
   const addScore= () => score++;
 
@@ -49,10 +71,6 @@ playerOne.addScore();
 playerOne.getScore();
 const score=document.getElementById("score");
 
-while (playerOne.getScore()<5) {
-  let currentScore=playerOne.getScore();
-  score.textContent=currentScore;
-}
 
 
 if (playerOne.getScore()>5){
